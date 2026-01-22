2da83cb31ae9f53b29d9790cd22ba9df75504fb2
Tssuyu <asatssuyu@gmail.com>
2026-01-22 16:06:21 +0500

    refactored XMLHTTPRequest -> fetch API

--- js/script.js
+++ js/script.js
@@ -236,27 +236,27 @@ window.addEventListener('DOMContentLoaded', () => {
       form.insertAdjacentElement('afterend', statusMessage);
 
       const formData = new FormData(form);
-      const request = new XMLHttpRequest();
-
-      request.open('POST', 'server.php');
-      request.setRequestHeader('Content-type', 'application/json, charset=utf-8');
-
       const object = {};
       formData.forEach(function(key, value) {
         object[key] = value;
       });
 
-      request.send(JSON.stringify(object));
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          console.log(request.response);
-          showThanksModal(message.success);
-          form.reset();
-          statusMessage.remove();
-        } else {
-          showThanksModal(message.failure);
-        }
+      fetch('server1.php', {
+        method: "POST",
+        headers: {
+          'Content-type': 'application/json, charset=utf-8'
+        },
+        body: JSON.stringify(object)
+      })
+      .then(data => data.text())
+      .then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.failure);
+      }).finally(() => {
+        form.reset();
       })
     });
   }
