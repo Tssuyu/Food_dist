f2a379ca21714498deda5608918075776cba3dd4
Tssuyu <asatssuyu@gmail.com>
2026-02-23 12:21:42 +0500

    add Slider (2nd version)

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modal
 
   const modalOpenBtns = document.querySelectorAll('[data-modal]'),
-        modalWindow = document.querySelector('.modal');
+    modalWindow = document.querySelector('.modal');
 
   function closeModal() {
     modalWindow.classList.add('hide');
@@ -183,7 +183,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  async function getResource (url) {
+  async function getResource(url) {
     let res = await fetch(url);
 
     if (!res.ok) {
@@ -194,24 +194,24 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   getResource('http://localhost:3002/menu')
-  .then(data => {
-    data.forEach(({img, altimg, title, descr, price}) => {
-      new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+    .then(data => {
+      data.forEach(({ img, altimg, title, descr, price }) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+      })
     })
-  })
 
   // Forms
 
   const forms = document.querySelectorAll('form'),
-        message = {
-          loading: 'img/form/spinner.svg',
-          success: 'Спасибо! Скоро мы с вами свяжемся.',
-          failure: 'Что-то пошло не так...'
-        }
+    message = {
+      loading: 'img/form/spinner.svg',
+      success: 'Спасибо! Скоро мы с вами свяжемся.',
+      failure: 'Что-то пошло не так...'
+    }
 
   forms.forEach(form => {
     bindPostData(form);
-  });      
+  });
 
   const postData = async (url, data) => {
     const res = await fetch(url, {
@@ -237,21 +237,18 @@ window.addEventListener('DOMContentLoaded', () => {
       form.insertAdjacentElement('afterend', statusMessage);
 
       const formData = new FormData(form);
-      const object = {};
-      formData.forEach(function(value, key) {
-        object[key] = value;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      postData('http://localhost:3002/requests', JSON.stringify(object))
-      .then(data => {
-        console.log(data);
-        showThanksModal(message.success);
-        statusMessage.remove();
-      }).catch(() => {
-        showThanksModal(message.failure);
-      }).finally(() => {
-        form.reset();
-      })
+      postData('http://localhost:3002/requests', json)
+        .then(data => {
+          console.log(data);
+          showThanksModal(message.success);
+          statusMessage.remove();
+        }).catch(() => {
+          showThanksModal(message.failure);
+        }).finally(() => {
+          form.reset();
+        })
     });
   }
 
@@ -278,5 +275,103 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000)
   }
+
+  //  Slider
+
+  const slider = document.querySelector('.offer__slider'),
+    prevBtn = document.querySelector('.offer__slider-prev'),
+    nextBtn = document.querySelector('.offer__slider-next'),
+    slides = document.querySelectorAll('.offer__slide'),
+    currentSlideNum = document.getElementById('current'),
+    totalSlideCount = document.getElementById('total'),
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    slidesField = document.querySelector('.offer__slider-inner'),
+    width = window.getComputedStyle(slidesWrapper).width,
+    dotsWrapper = document.createElement('ol');
+  let slideIndex = 1;
+  offset = 0;
+
+  slider.style.position = 'relative';
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+  slidesWrapper.style.overflow = 'hidden';
+
+  totalSlideCount.textContent = (slides.length < 10) ? `0${slides.length}` : slides.length;
+  currentSlideNum.textContent = '01';
+
+  dotsWrapper.classList.add('carousel-indicators');
+  slider.append(dotsWrapper);
+
+  slides.forEach((slide, i) => {
+    slide.style.width = width;
+
+    let dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add('dot');
+    dotsWrapper.append(dot);
+  })
+
+  function showActiveDot(i) {
+    dotsWrapper.querySelectorAll('li').forEach((dot) => {
+      dot.classList.remove('active');
+    })
+    const activeDot = dotsWrapper.querySelector(`[data-slide-to="${i}"]`);
+    activeDot.classList.add('active');
+  }
+
+  function showNextSlide() {
+    if (offset === parseInt(width) * (slides.length - 1)) {
+      offset = 0;
+      currentSlideNum.textContent = '01';
+    } else {
+      offset += parseInt(width);
+      currentSlideNum.textContent = (currentSlideNum.textContent < 9) ? `0${parseInt(currentSlideNum.textContent) + 1}` : parseInt(currentSlideNum.textContent) + 1;
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+    showActiveDot(parseInt(currentSlideNum.textContent));
+  }
+
+  function showPrevSlide() {
+    if (offset === 0) {
+      offset = parseInt(width) * (slides.length - 1);
+      currentSlideNum.textContent = totalSlideCount.textContent;
+    } else {
+      offset -= parseInt(width);
+      currentSlideNum.textContent = (currentSlideNum.textContent < 11) ? `0${parseInt(currentSlideNum.textContent) - 1}` : parseInt(currentSlideNum.textContent) - 1;
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+    showActiveDot(parseInt(currentSlideNum.textContent));
+  }
+
+  showActiveDot(parseInt(currentSlideNum.textContent));
+
+  dotsWrapper.querySelectorAll('li').forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+      offset = parseInt(width) * i;
+      slidesField.style.transform = `translateX(-${offset}px)`;
+      currentSlideNum.textContent = (i < 9) ? `0${i + 1}` : i + 1;
+      showActiveDot(i + 1);
+    })
+  })
+
+  nextBtn.addEventListener('click', () => {
+    showNextSlide();
+  })
+
+  prevBtn.addEventListener('click', () => {
+    showPrevSlide();
+  })
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      showNextSlide();
+    }
+    if (event.key === 'ArrowLeft') {
+      showPrevSlide();
+    }
+  });
+
+
 
 });
